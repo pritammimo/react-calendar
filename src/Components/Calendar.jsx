ef3044591ec396d7f450d479f7f6d663ddafa52a
pritamcn <crpritamsaha@gmail.com>
2023-04-17 22:45:30 +0530

    first commit

---
+++ src/Components/Calendar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"
+const Calendar = () => {
+  let  handleDateClick = (arg) => { // bind with an arrow function
+        alert(arg.dateStr)
+      }
+  return (
+    <FullCalendar
+  plugins={[ dayGridPlugin,interactionPlugin ]}
+  initialView="dayGridMonth"
+  dateClick={handleDateClick}
+  weekends={true}
+  events={[
+    { title: 'event 1', date: '2023-04-17' },
+    { title: 'event 2', date: '2023-04-15' }
+  ]}
+/>
+  )
+}
+
+export default Calendar
